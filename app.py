2e45bd72586555a041a17bfb8388426654fd8b8d
ark <>
2023-09-29 14:43:52 -0700

    Fix pymongo import, begin id code

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask
-import pymongo
+from flask import Flask, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-@app.route("/api/makecard")
+@app.route("/api/getid", methods=["POST"])
+def getid():
+    id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
+
+@app.route("/api/makecard",methods=["POST"])
 def makecard():
+    requestinput = request.data
     return "Card"
