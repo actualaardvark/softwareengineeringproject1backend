a95f393d99f956902a616ffbfe6b2a31992ee9f3
ark <>
2023-10-02 20:47:23 -0700

    Create apitests.sh

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
 from flask import Flask, request
 from pymongo import MongoClient
+import random
+import string
 
 app = Flask(__name__)
 
 mongoclient = MongoClient()
 
-@app.route("/api/getid", methods=["POST"])
+database = mongoclient["db"]
+
+cards = database["cards"]
+
+idlength = 16
+
+@app.route("/api/getid", methods=["POST", "GET"])
 def getid():
-    id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
+    id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=idlength)).lower()
+    print(id)
+    print(cards.find_one({'id': id}))
+        #id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=idlength))
+    return id
 
 @app.route("/api/makecard",methods=["POST"])
 def makecard():
-    requestinput = request.data
-    return "Card"
+    requestinput = request.get_json()
+    print(requestinput)
+    id = requestinput["id"]
+    title = requestinput["title"]
+    description = requestinput["description"]
+    difficulty = requestinput["difficulty"]
+    return "success"
