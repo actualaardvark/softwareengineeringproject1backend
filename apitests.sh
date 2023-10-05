33a8ab1239136c114f778b2b4bc3ab0e2038cd34
ark <>
2023-10-05 15:35:59 -0700

    In progress refactor of apitests.sh

--- apitests.sh
+++ apitests.sh
@@ -5,43 +5,31 @@ function pausetests {
     sleep 1
 }
 
+function sendtestrequest {
+   if [ "$2" == "$(sh ./testcases/$1.sh)" ]; then
+      echo "[PASS] $1"
+   else
+      echo "[FAIL] $1"
+   fi
+}
+
 echo '***Test makecard API***'
 
-echo "Testing with Valid Card"
-# No Error
-curl -s \
-   -X POST http://127.0.0.1:5000/api/makecard \
-   -H 'Content-Type: application/json' \
-   -d '{"id":"2nt3q3ju40t2tepj","title":"Hellow World", "description":"Insert Description Here", "difficulty":"5"}'; echo
+sendtestrequest "makecard/valid" "success"
 
 pausetests
 
-echo "Testing with Additional Key"
-# Error
-curl -s \
-   -X POST http://127.0.0.1:5000/api/makecard \
-   -H 'Content-Type: application/json' \
-   -d '{"id":"2nt3q3ju40t2tepj","title":"Hellow World", "description":"Insert Description Here", "difficulty":"5", "extra":"95"}'; echo
-
+sendtestrequest "makecard/additionalkey" "keyschemavalidationerror"
 
 pausetests
 
-echo "Testing with Missing Title Key"
-# Error
-curl -s \
-   -X POST http://127.0.0.1:5000/api/makecard \
-   -H 'Content-Type: application/json' \
-   -d '{"id":"2nt3q3ju40t2tepj", "description":"Insert Description Here", "difficulty":"5"}'; echo
-
+sendtestrequest "makecard/missingtitle" "keyschemavalidationerror"
 
 pausetests
 
 echo "Testing with Missing Description Key"
-# Error
-curl -s \
-   -X POST http://127.0.0.1:5000/api/makecard \
-   -H 'Content-Type: application/json' \
-   -d '{"id":"2nt3q3ju40t2tepj","title":"Hellow World", "difficulty":"5"}'; echo
+
+sendtestrequest "makecard/missingdescription" "keyschemavalidationerror"
 
 pausetests
 
@@ -55,11 +43,6 @@ curl -s \
 pausetests
 
 echo "Testing with Missing Difficulty Key"
-# Error
-curl -s \
-   -X POST http://127.0.0.1:5000/api/makecard \
-   -H 'Content-Type: application/json' \
-   -d '{"id":"2nt3q3ju40t2tepj","title":"Hellow World", "description":"Insert Description Here"}'; echo
 
 pausetests
 
@@ -90,7 +73,12 @@ curl -s \
 
 pausetests
 
-echo "Testing with GET Request"
 # Error
 # Source https://superuser.com/questions/272265/getting-curl-to-output-http-status-code
-curl -s -o /dev/null -w "%{http_code}" "http://127.0.0.1:5000/api/makecard"; echo
+outputcode="$(curl -s -o /dev/null -w '%{http_code}' 'http://127.0.0.1:5000/api/makecard')"
+echo $outputcode
+if [ $outputcode == "405" ]; then
+   echo "[PASS] Testing with GET Request"
+else
+   echo "[FAIL] Testing with GET Request"
+fi
---
+++ autoescape.py
@@ -0,0 +1,1 @@
+print("\n\n" + str(input("Insert Command: ")).replace('"', r'\"'))
