081637b81dbfdc3bb1d7f7330c09134f37b3f201
ark <>
2023-10-10 22:21:48 -0700

    Remove commented testcase script

--- apitests.sh
+++ apitests.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-
-# function pausetests {
-#     sleep 1
-# }
-
-# function sendtestrequest {
-#    if [ "$2" == "$(sh ./testcases/$1.sh)" ]; then
-#       echo "[PASS] $1"
-#    else
-#       echo "[FAIL] $1"
-#    fi
-# }
-
 for folder in $(ls ./testcases) 
 do 
    echo -e "\n--- Testing $folder API ---\n"
@@ -24,31 +11,3 @@ do
       fi
    done
 done
-
-# echo '***Test makecard API***'
-# sendtestrequest "makecard/valid" "success"
-# pausetests
-# sendtestrequest "makecard/additionalkey" "pass"
-# pausetests
-# sendtestrequest "makecard/missingtitle" "keyschemavalidationerror"
-# pausetests
-# sendtestrequest "makecard/missingdescription" "keyschemavalidationerror"
-# pausetests
-# sendtestrequest "makecard/missingid" "keyschemavalidationerror"
-# pausetests
-# sendtestrequest "makecard/missingdifficulty" "keyschemavalidationerror"
-# pausetests
-# sendtestrequest "makecard/invaliddifficulty" "difficultyvalueerror"
-# pausetests
-# sendtestrequest "makecard/invalidid" "idlengtherror"
-# pausetests
-# sendtestrequest "makecard/blankjson" "keyschemavalidationerror"
-# pausetests
-# sendtestrequest "makecard/getrequest" "405"
-
-# echo '***Test getid API***'
-# sendtestrequest "getid/correctlengthcheck" "pass"
-# pausetests
-# sendtestrequest "getid/onlynumberscheck" "pass"
-# pausetests
-# sendtestrequest "getid/onlytextcheck" "pass"
