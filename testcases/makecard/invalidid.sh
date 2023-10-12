fbe30cbfcd1a6a31f8f4129c846f57bcc4b6e88d
ark <>
2023-10-11 17:44:45 -0700

    Remove newline

--- testcases/makecard/invalidid.sh
+++ testcases/makecard/invalidid.sh
@@ -1,6 +1,4 @@
 output=$(curl -s -X POST http://127.0.0.1:5000/api/makecard -H 'Content-Type: application/json' -d '{"id":"23nt","title":"Hellow World", "description":"Insert Description Here", "difficulty":"5"}')
-
-
 if [[ $output == "idlengtherror" ]]; then
    echo "pass"
 else
