ba00418ea0bd347c68227c9446fac8527a34d1d0
ark <>
2023-10-14 18:59:35 -0700

    Remove curl output for generatecards.sh

--- generatecards.sh
+++ generatecards.sh
@@ -1,4 +1,4 @@
 for i in {1..5}; do
    output=$(curl -s -X POST http://127.0.0.1:5000/api/getid)
-   curl -s -X POST http://127.0.0.1:5000/api/makecard -H 'Content-Type: application/json' -d '{"id":"'"$output"'","title":"Hellow World", "description":"Insert Description Here", "difficulty":"'"$(shuf -i1-10 -n1)"'"}'
+   curl -s -X POST http://127.0.0.1:5000/api/makecard -H 'Content-Type: application/json' -d '{"id":"'"$output"'","title":"Hellow World", "description":"Insert Description Here", "difficulty":"'"$(shuf -i1-10 -n1)"'"}' > /dev/null
 done
