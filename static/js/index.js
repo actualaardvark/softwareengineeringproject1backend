8fc2b91c9a10daaf8d6a987d6bc0f98a89046e32
ark <>
2023-10-14 18:14:20 -0700

    Remove sleep function

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,5 @@
 var beginningscore = 20;
 
-function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-      if ((new Date().getTime() - start) > milliseconds){
-        break;
-      }
-    }
-  }
-
 async function getCards(){
     document.getElementById('body').innerHTML = ""
     const response = await fetch("http://127.0.0.1:5000/api/getcards", {method: "POST"});
