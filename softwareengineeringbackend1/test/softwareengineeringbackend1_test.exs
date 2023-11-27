b7504452d41f8a82b2ca40856c77d0c30d665644
ark-lamp-umbrella <>
2023-11-26 16:55:43 -0800

    delete basic elixir files

--- softwareengineeringbackend1/test/softwareengineeringbackend1_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Softwareengineeringbackend1Test do
-  use ExUnit.Case
-  doctest Softwareengineeringbackend1
-
-  test "greets the world" do
-    assert Softwareengineeringbackend1.hello() == :world
-  end
-end
